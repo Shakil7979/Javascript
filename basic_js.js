c0beab6b09cf007fbb879219b26166aa69ba7f74
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-19 22:47:47 +0600

    Create basic_js.js

---
+++ basic_js.js
@@ -0,0 +1,7 @@
+
+// How to show javascript data in HTML  
+
+document.write() // html er body te show korar jnno 
+console.log() // console e data show korar jnno 
+window.alert() // window te data alert show korar jnno 
+alert() // alert a show korar jnno

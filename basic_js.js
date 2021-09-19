e5802f98b6a49fa508b6230c730ad4c5e25a7738
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-20 00:06:36 +0600

    Update basic_js.js

--- basic_js.js
+++ basic_js.js
@@ -1,7 +1,24 @@
 
+//*********************************************** Javascript Basic *******************************************************
+ 
 // How to show javascript data in HTML  
-
 document.write() // html er body te show korar jnno 
 console.log() // console e data show korar jnno 
 window.alert() // window te data alert show korar jnno 
 alert() // alert a show korar jnno
+
+
+// Data type
+typeof() // datar type show korar jnno use kora hoy
+
+// data type change korar jnno 
+number('shakil') // string theke number a convert kora jay
+toString() // number, float theke string a convert kora jabe 
+parseFloat()  // strign, number theke float convert kora jabe
+parseInt()   // string , number , float theke integer a convert kora jabe
+
+toFixed(2)  // 2.564 doshomik er porer 2 string show korbe ex: 56
+toPrecision(2)  // 2.564 total 2 ta string show korbe ex: 2.5
+
+prompt()  // input alert show korbe
+value.length   // cherector length show korbe

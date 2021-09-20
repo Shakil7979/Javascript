f95a14f383b366a64be39005897553a448e734f0
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-20 16:55:52 +0600

    Update for_loop.js

--- for_loop.js
+++ for_loop.js
@@ -3,3 +3,10 @@ var x =  1;
 for(x; x <= 5; x++ ){
     document.write('tumi parbe' + '<br>');
 }
+
+// 1 theke 100 porjunto Bejor sonkha ber korar niom  
+var y =  1;
+
+for(y; y <= 99; y = y+2 ){
+    document.write(' '+ y);
+} 

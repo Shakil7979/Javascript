ebec618ec9aa58b3f3a9930637dd31741f524ae5
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-20 16:27:36 +0600

    Create if_else.js

---
+++ if_else.js
@@ -0,0 +1,34 @@
+  
+
+  var number = parseInt(prompt('Enter your Number : ')); 
+
+  if(number == 1)
+    document.getElementById('output').innerHTML= 'one';
+
+  else if(number == 2)
+    document.getElementById('output').innerHTML= 'two'; 
+
+  else if(number == 3)
+    document.getElementById('output').innerHTML= 'three'; 
+
+  else if(number == 4)
+    document.getElementById('output').innerHTML= 'four'; 
+
+  else if(number == 5)
+    document.getElementById('output').innerHTML= 'five'; 
+
+  else if(number == 6)
+    document.getElementById('output').innerHTML= 'six'; 
+
+  else if(number == 7)
+    document.getElementById('output').innerHTML= 'seven'; 
+
+  else if(number == 8)
+    document.getElementById('output').innerHTML= 'eight'; 
+
+  else if(number == 9)
+    document.getElementById('output').innerHTML= 'nine'; 
+
+  else
+    document.getElementById('output').innerHTML= 'Zero'; 
+

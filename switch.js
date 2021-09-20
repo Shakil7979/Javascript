e450c7ea813537ac2a5298eaaae0d4d8544b839a
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-20 16:24:31 +0600

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,45 @@
+  
+
+  var number = parseInt(prompt('Enter your Number : '));
+  
+  switch (number) {
+      case 1: 
+          document.getElementById('output').innerHTML= 'one'; 
+          break;
+  
+      case 2:  
+          document.getElementById('output').innerHTML= 'two'; 
+          break;
+  
+      case 3: 
+          document.getElementById('output').innerHTML= 'three'; 
+          break;
+  
+      case 4: 
+          document.getElementById('output').innerHTML= 'four'; 
+          break;
+  
+      case 5: 
+          document.getElementById('output').innerHTML= 'five'; 
+          break;
+  
+      case 6: 
+          document.getElementById('output').innerHTML= 'six'; 
+          break;
+  
+      case 7: 
+          document.getElementById('output').innerHTML= 'seven'; 
+          break;
+
+      case 8: 
+          document.getElementById('output').innerHTML= 'eight'; 
+          break;
+
+      case 9: 
+          document.getElementById('output').innerHTML= 'nine'; 
+          break;
+  
+      default: 
+        document.getElementById('output').innerHTML= 'Zero'; 
+          break;
+  }

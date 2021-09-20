f1a65cdf3e4d4e1d2c4c97e632c778e6694716df
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-21 02:07:10 +0600

    Create ternary_condition.js

---
+++ ternary_condition.js
@@ -0,0 +1,10 @@
+
+
+// 3 ti sonkhar moddhe boro sonkha ber koro using to ternary condition
+
+var x = 50;
+var y = 100;
+var z = 20;
+
+var result = x > y ? "X boro" : y > z ? "y boro" : "z boro";
+document.write(result);

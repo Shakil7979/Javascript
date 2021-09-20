a40115287cc2fb214ddbe78852f085e106bb48ce
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-21 01:38:04 +0600

    Create while_loop.js

---
+++ while_loop.js
@@ -0,0 +1,13 @@
+// 1 theke 100 porjunto 3 and 5 dara vag korle vag fol sunno hobe sei sonkkha gulo ber koro
+
+var x = 1;
+var sum = 0;
+while (x <= 20) {
+  if (x % 3 == 0 && x % 5 == 0) {
+    document.write(" " + x);
+    sum = sum + x;
+  }
+  x = x + 1;
+}
+
+document.write(" sum = " + sum);

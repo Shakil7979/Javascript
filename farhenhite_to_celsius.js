92735b4b8b79f422cffb5b0a59ebf537c8d676b8
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-20 16:10:25 +0600

    Create farhenhite_to_celsius.js

---
+++ farhenhite_to_celsius.js
@@ -0,0 +1,8 @@
+// farenhite to celsius 
+var farn = parseFloat(prompt('Enter Now Farenhite : ')); 
+var cels = (farn - 32) * (5/9); 
+document.write(cels);
+
+var cels = parseFloat(prompt('Enter Now celsius : ')); 
+var farn = (cels * ()5/9) - 32; 
+document.write(farn);

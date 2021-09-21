07f724d3d723e0fc791969ea6803c36af379b86e
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-21 18:13:46 +0600

    Create function_calc.js

---
+++ function_calc.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+// jog function 
+
+function jog(x,y){
+    var result = x + y;
+    return result;
+}
+
+console.log(jog(60,7));
+
+// biog 
+function  biog(x,y){
+    var result = x - y;
+    return result;
+}
+
+console.log(biog(5,5));
+
+// gun 
+function gun(x,y){
+    var result = x * y;
+    return result;
+}
+
+console.log(gun(5,5));
+
+
+// vag
+function vag(x,y){
+    var result = x / y;
+    return result;
+}
+
+console.log(vag(5,5));
+

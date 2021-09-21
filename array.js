6bd19584bd67d93459b03a8d3359e0c5cb15d60c
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-21 17:56:44 +0600

    Create array.js

---
+++ array.js
@@ -0,0 +1,22 @@
+
+
+
+// array 
+
+var list = new Array(); 
+    list[0] = 'shakil';
+    list[1] = 'pushon';
+    list[2] = 'dada';
+    list[3] = 'dadu';
+    list[4] = 'shanto';
+
+document.write(list + "<br>"); 
+
+var names = ["shakil","pushon","dada","dadu","shanto"];
+names.push("rayhan");
+names.push("alif"); 
+document.write(names+"<br>");
+names.pop('alif'); 
+document.write(names);
+
+

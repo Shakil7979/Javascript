d8564c7dffbee7eb24678290e60fccb0d0e302e0
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-22 10:39:32 +0600

    Create array_sum.js

---
+++ array_sum.js
@@ -0,0 +1,15 @@
+
+
+var numbers = [5,10,20,30,50,10,15];  
+ 
+var sum = 0; 
+
+for(var x = 0; x<numbers.length; x++){
+    var number = numbers[x];
+    sum = sum + number;
+
+} 
+
+document.write("sum = "+sum);
+
+

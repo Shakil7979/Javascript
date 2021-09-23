cb8377610dd4db7f4117004fdee96d805dab729c
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-23 22:41:00 +0600

    Create math.js

---
+++ math.js
@@ -0,0 +1,14 @@
+
+Math.sqrt(25) // squre value show korbe
+Math.abs(-5)   // positive value pas korbe
+Math.sin(2)  // sin thita er man ber korbe
+Math.pow(2,6)  // power er value ber korbe
+Math.ceil(3.3) // 4 er purno sonkha ber korbe
+Math.floor(3.6)   // 3 er purno sonkha ber korbe
+Math.round(3.5)  // (3.4) hole 3 purno sonkha hbe, (3.5) er upore hole 4 purno sonkha ber korbe
+Math.max(0,12) // maximum number ber korbe 
+Math.random() * 7 // random 0 theke 1 er moddhe random korbe.. kokhono 1 hbe na. and 7 er sathe gun hobe kintu 7 hbe na 
+Math.floor(Math.random() * 10)+1; // 10 theke 20 porjunto random number create
+
+
+

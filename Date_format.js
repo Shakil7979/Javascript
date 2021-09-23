7b6528f9ffb2dfeb686422ec78149af5038aacd7
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-09-24 00:08:38 +0600

    Create Date_format.js

---
+++ Date_format.js
@@ -0,0 +1,23 @@
+
+
+var date = new Date();
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month);
+
+var c_date = date.getDate();
+console.log(c_date);
+
+var day = date.getDay();
+console.log(day);
+
+var hour = date.getHours();
+console.log(hour);
+
+var miniute = date.getMinutes();
+console.log(miniute);
+
+

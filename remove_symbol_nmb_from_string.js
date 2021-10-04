4aff8bc9d64a40a349eb9fa42baafa09be1d492e
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-10-04 13:33:02 +0600

    Create remove_symbol_nmb_from_string.js

---
+++ remove_symbol_nmb_from_string.js
@@ -0,0 +1,12 @@
+
+
+// get String from string theke
+let myString = "sh2222a3$2kil";
+console.log(myString.replace(/[^a-zA-Z ]/g, ""));
+
+
+// get number string theke
+let myString = "sh2222a3$2kil";
+console.log(myString.replace(/\D/g, ""));
+
+

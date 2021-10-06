a3fd9ca9d69f7d381fc812d6fc5f05eec9cd6413
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-10-06 23:24:29 +0600

    Create question_answer.js

---
+++ question_answer.js
@@ -0,0 +1,5 @@
+
+array : different elements nia akti array hoy.
+object : একটি বস্তু বৈশিষ্ট্য এবং প্রকার সহ একটি স্বতন্ত্র সত্তা।  an object is a standalone entity, with properties and type.
+lenght : string property.
+

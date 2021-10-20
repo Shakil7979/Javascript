07a036f0b0c5b6f5860e99fcfaa2a09289073fa4
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2021-10-20 20:21:41 +0600

    Create import_and_export.js

---
+++ import_and_export.js
@@ -0,0 +1,11 @@
+
+// must be add type module
+<script src="script.js" type="module"></script>
+
+
+// index.js
+import { text } from "./module.js";
+
+// module.js
+export let text = "Hi Sadbin Shakil";
+
